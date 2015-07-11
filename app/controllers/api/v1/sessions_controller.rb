a0e7f5714707ea957f6e9bd19b32d03d03233e0f
galeno <lenghan1991@gmail.com>
2015-07-11 23:42:09 +0800

    fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::SessionsController <  Api::V1::BaseController
   def create
-    @user = User.find_by(email: params[:email])
+    @user = if params[:loginName].include? '@'
+      User.find_by(email: params[:loginName])
+    else
+      User.find_by(login: params[:loginName])
+    end
+
     if @user && @user.authenticate(params[:password])
       self.current_user = @user
     else
